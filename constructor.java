e156af644fe3377e5a3bf9bc32cff505ba9eed76
eunice <eunicewanjiru208@gmail.com>
2024-02-19 23:53:00 +0300

    Implement main method

--- constructor.java
+++ constructor.java
@@ -16,7 +16,7 @@ import java.util.Scanner;
  }
 
 
-// public static void main(String [] args){
+ public static void main(String [] args){
 // Scanner sc = new Scanner(System.in);
 // System.out.println("Enter the first number: ");
 
