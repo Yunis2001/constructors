ece2a892c0a5d2b27cfd3ddf088a1ca6a4ecefca
eunice <eunicewanjiru208@gmail.com>
2024-02-19 23:54:49 +0300

    Initialize Scanner object for user input handling

--- constructor.java
+++ constructor.java
@@ -17,10 +17,10 @@ import java.util.Scanner;
 
 
  public static void main(String [] args){
-// Scanner sc = new Scanner(System.in);
-// System.out.println("Enter the first number: ");
+ Scanner sc = new Scanner(System.in);
 
-// int n1 = sc.nextInt();
+// System.out.println("Enter the first number: ");
+ //int n1 = sc.nextInt();
 // System.out.println("Enter the second number: ");
 // int n2 = sc.nextInt();
 
