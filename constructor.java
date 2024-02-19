ec97db888f751425374a84019bc33e1ae738a706
eunice <eunicewanjiru208@gmail.com>
2024-02-19 23:56:01 +0300

    use system out println to Enter the first integer

--- constructor.java
+++ constructor.java
@@ -19,8 +19,9 @@ import java.util.Scanner;
  public static void main(String [] args){
  Scanner sc = new Scanner(System.in);
 
-// System.out.println("Enter the first number: ");
- //int n1 = sc.nextInt();
+ System.out.println("Enter the first number: ");
+ int n1 = sc.nextInt();
+ 
 // System.out.println("Enter the second number: ");
 // int n2 = sc.nextInt();
 
